fa0463e852f78edbfcc72c03a6a370199bc888d3
edvalenzuela <edvalenzuelap@gmail.com>
2023-01-13 21:25:39 -0300

    🎨 add linkedlist, compare string, poo refactor

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,8 +1,25 @@
 import { Sorter } from "./Sorter";
 import { NumberCollection } from "./NumberCollection";
+import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
+
+/* const numberCollection = new NumberCollection([10, 3, -5, 0]);
+numberCollection.sort();
+console.log(numberCollection.data); */
+
+/* const charactersCollection = new CharactersCollection('Eduardo');
+charactersCollection.sort();
+console.log(charactersCollection.data); */
+
+const linkedList = new LinkedList();
+linkedList.add(500)
+linkedList.add(-10)
+linkedList.add(-3)
+linkedList.add(4)
+
+linkedList.sort();
+linkedList.print()
 
 
-const numberCollection = new NumberCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numberCollection);
-sorter.sort();
-console.log(numberCollection.data);
+
+
